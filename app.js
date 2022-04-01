87da82ad88bc0a34d23c4c33cfa7eae15f857ef1
whOmp <sean.rogers.ys@gmail.com>
2022-04-01 23:03:14 +1100

    added some styling updated seeding

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const methodOverride = require('method-override');
 const { default: mongoose } = require('mongoose');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -17,6 +18,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20 ) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            price,
+            image: 'https://images.unsplash.com/photo-1473294312123-83488e2f8e8f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHw0ODQzNTF8fHx8fHx8MTY0ODgxMzczNQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sint quaerat excepturi doloribus inventore nam unde porro quo? Sed veniam quod nesciunt, est aut doloribus aperiam recusandae illo deserunt quos.',
         })
         await camp.save();
     }
