e700f7f0a94e58f7a32cffd5c4c555976359179a
whOmp <sean.rogers.ys@gmail.com>
2022-04-09 14:55:52 +1000

    error handling with joi

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const path = require('path');
 const methodOverride = require('method-override');
 const { default: mongoose } = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -26,6 +29,16 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req,res,next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.send('HELLO FROM YELP CAMP!')
 })
@@ -39,7 +52,8 @@ app.get('/campgrounds/new', (req,res) => {
     res.render('campgrounds/new')
 });
 
-app.post('/campgrounds', catchAsync( async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync( async (req, res) => {
+        // if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
         const campground = new Campground(req.body.campground);
         await campground.save();
         res.redirect(`/campgrounds/${campground._id}`);
@@ -55,7 +69,7 @@ app.get('/campgrounds/:id/edit', catchAsync( async (req,res) => {
     res.render('campgrounds/edit', { campground });
 }));
 
-app.put('/campgrounds/:id', catchAsync( async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync( async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
@@ -68,11 +82,13 @@ app.delete('/campgrounds/:id', catchAsync( async (req,res) => {
 }));
 
 app.all('*', (req, res, next) => {
-    res.send("404")
+    res.send(new ExpressError('Page not found', 404));
 })
 
 app.use((err, req, res, next) => {
-    res.send('Oh Boy, Somethig went wrong!');
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    if (!err.message) err.message = 'Oh no its all fucked!';
+    res.status(statusCode).render('error', { err })
 });
 
 app.listen(3000, () => {
